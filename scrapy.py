07af27d6db773d9a15c85c73bc1d2096209735f0
chloezzhang215 <90955638+chloezzhang215@users.noreply.github.com>
2021-12-09 23:55:20 -0800

    Add files via upload

--- scrapy.py
+++ scrapy.py
@@ -1,101 +1,92 @@
-import scrapy
+import requests, time, csv
+from bs4 import BeautifulSoup
 
 
-class HotelSentimentItem(scrapy.Item):
-    title = scrapy.Field()
-    content = scrapy.Field()
-    stars = scrapy.Field()
+url_list = []
 
 
-class TripAdvisorReviewItem(scrapy.Item):
-    title = scrapy.Field()
-    content = scrapy.Field()
-    review_stars = scrapy.Field()
+headers = {'Referer':'https://www.booking.com/hotel/us/indigo-los-angeles-downtown.html?aid=304142;ucfs=1&',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
 
-    reviewer_id = scrapy.Field()
-    reviewer_name = scrapy.Field()
-    reviewer_level = scrapy.Field()
-    reviewer_location = scrapy.Field()
+# for i in range(1,6):
+    # params = {
+    # 'aid': '304142',
+    # 'label': 'gen173nr-1DCAso7AFCG2luZGlnby1sb3MtYW5nZWxlcy1kb3dudG93bkgzWARoiQKIAQGYATG4ARfIAQzYAQPoAQH4AQaIAgGoAgO4Auy7-IsGwAIB0gIkMzczNzA1OWEtODhmMS00NDVmLTg1ZjItZTQ3Mjk0N2MzZjBi2AIE4AIB',
+    # 'sid': '42a1d99d02d86ba180f9f73b69ad4373',
+    # 'cc1': 'us',
+    # 'dist': '1',
+    # 'length_of_stay': '1',
+    # 'pagename': 'indigo-los-angeles-downtown',
+    # 'type': 'total',
+    # 'offset': str(i*10),
+    # 'rows': '10',
+    # '_': '1635655153109'
+    # }
+for i in range(1, 700):
 
-    city = scrapy.Field()
-
-    hotel_name = scrapy.Field()
-    hotel_url = scrapy.Field()
-    hotel_classs = scrapy.Field()
-    hotel_address = scrapy.Field()
-    hotel_locality = scrapy.Field()
-    hotel_review_stars = scrapy.Field()
-    hotel_review_qty = scrapy.Field()
-
-
-class BookingReviewItem(scrapy.Item):
-    title = scrapy.Field()
-    score = scrapy.Field()
-    positive_content = scrapy.Field()
-    negative_content = scrapy.Field()
-    tags = scrapy.Field()
-
-
-
-import scrapy
-from scrapy.loader import ItemLoader
-from hotel_sentiment.items import BookingReviewItem
-
-#crawl up to 6 pages of review per hotel
-max_pages_per_hotel = 6
-
-class BookingSpider(scrapy.Spider):
-    name = "booking"
-    start_urls = [
-        #"http://www.booking.com/searchresults.html?aid=357026&label=gog235jc-city-XX-us-newNyork-unspec-uy-com-L%3Axu-O%3AosSx-B%3Achrome-N%3Ayes-S%3Abo-U%3Ac&sid=b9f9f1f142a364f6c36f275cfe47ee55&dcid=4&city=20088325&class_interval=1&dtdisc=0&from_popular_filter=1&hlrd=0&hyb_red=0&inac=0&label_click=undef&nflt=di%3D929%3Bdistrict%3D929%3B&nha_red=0&postcard=0&redirected_from_city=0&redirected_from_landmark=0&redirected_from_region=0&review_score_group=empty&room1=A%2CA&sb_price_type=total&score_min=0&ss_all=0&ssb=empty&sshis=0&rows=15&tfl_cwh=1",
-        #add your city url here
-    ]
-
-    pageNumber = 1
-
-    #for every hotel
-    def parse(self, response):
-        for hotelurl in response.xpath('//a[@class="hotel_name_link url"]/@href'):
-            url = response.urljoin(hotelurl.extract())
-            yield scrapy.Request(url, callback=self.parse_hotel)
-
-        next_page = response.xpath('//a[starts-with(@class,"paging-next")]/@href')
-        if next_page:
-            url = response.urljoin(next_page[0].extract())
-            yield scrapy.Request(url, self.parse)
-
-    #get its reviews page
-    def parse_hotel(self, response):
-        reviewsurl = response.xpath('//a[@class="show_all_reviews_btn"]/@href')
-        url = response.urljoin(reviewsurl[0].extract())
-        self.pageNumber = 1
-        return scrapy.Request(url, callback=self.parse_reviews)
-
-    #and parse the reviews
-    def parse_reviews(self, response):
-        if self.pageNumber > max_pages_per_hotel:
-            return
-        for rev in response.xpath('//li[starts-with(@class,"review_item")]'):
-            item = BookingReviewItem()
-            #sometimes the title is empty because of some reason, not sure when it happens but this works
-            title = rev.xpath('.//a[@class="review_item_header_content"]/span[@itemprop="name"]/text()')
-            if title:
-                item['title'] = title[0].extract()
-                positive_content = rev.xpath('.//p[@class="review_pos"]//span/text()')
-                if positive_content:
-                    item['positive_content'] = positive_content[0].extract()
-                negative_content = rev.xpath('.//p[@class="review_neg"]//span/text()')
-                if negative_content:
-                    item['negative_content'] = negative_content[0].extract()
-                item['score'] = rev.xpath('.//span[@itemprop="reviewRating"]/meta[@itemprop="ratingValue"]/@content')[0].extract()
-                #tags are separated by ;
-                item['tags'] = ";".join(rev.xpath('.//li[@class="review_info_tag"]/text()').extract())
-                yield item
-
-        next_page = response.xpath('//a[@id="review_next_page_link"]/@href')
-        if next_page:
-            self.pageNumber += 1
-            url = response.urljoin(next_page[0].extract())
-            yield scrapy.Request(url, self.parse_reviews)
+    #url = 'https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=el-royale-near-universal-studios-hollywood&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637637320104'
+    #url = 'https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=jolly-roger&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637638271985'
+    #url = ' https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=hollywood-inn-express-north&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637640125542'
+    #url = ' https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=sofitel-los-angeles&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637642228569'
+    #url = 'https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=kimpton-everly&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637647237662'
+    #url = ' https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=hollywood-the-of-hollywood&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637648057187'
+    #url = 'https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=venice-on-the-beach-venice&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637649058539'
+    #url = ' https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=level-furnished-living&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637650818847'
+    #url = 'https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=friendship-motor-inn&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637652141165'
+    #url = 'https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=days-inn-studio-city&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637652986072'
+    #url = 'https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=hostelling-international&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637653749570'
+    #url = ' https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=banana-bungalow-west-hollywood&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637654330107'
+    # url = 'https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=super-8-los-angeles-airport-lax&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637654941422'
+    url = 'https://www.booking.com/reviewlist.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAEruAEHyAEN2AED6AEBiAIBqAIDuAKA_OeMBsACAdICJDU1MWVmNzJlLTE1NjEtNDc1YS1iN2QyLWVlNmJkNjAwMzNmYtgCBOACAQ;sid=08b2e3d123f840696c87d000952ebe3c;srpvid=ca849d3386ba0058;sig=v1EC2A7cQC&;cc1=us&pagename=days-inn-hollywood-walk-of-fame&r_lang=&review_topic_category_id=&type=total&score=&sort=&dist=1&offset='+str(i*10)+'&rows=10&rurl=&text=&translate=&time_of_year=&_=1637655755199'
 
 
+
+    res = requests.get(url, headers = headers)
+
+    print(res.status_code)
+
+    bs = BeautifulSoup(res.text, 'html.parser')
+    score_list = bs.find_all('div', class_="bui-review-score__badge")
+    comment_list = bs.find_all('div',class_="bui-grid__column-10")
+    message_list = bs.find_all('div',class_="c-review")
+    
+    scores = []
+    for i in score_list:
+        scores.append(i.text)
+
+
+    titles = []
+    for comment in comment_list:
+        title = comment.find_all('h3')
+        for i in title:
+            titles.append(i.text.replace('\n', ''))
+
+    contents = []
+    for message in message_list:
+        content = message.find_all('span')
+        # print(content)
+        inner_list = []
+        for i in content:
+        
+            review = i.text.replace('·','').replace('Liked','').replace('Disliked','').replace('\xa0', '').strip()
+            if review != "This review has been hidden because it doesn't meet our guidelines." and review != '':
+
+                inner_list.append(review)
+        if len(inner_list) != 0:
+            contents.append(inner_list)
+
+    review_dict = {}
+    for i in range(len(titles)):
+        if len(titles) == len(contents):
+        
+            review_dict[titles[i]] = [contents[i],scores[i]]
+
+    # print(review_dict)
+
+    with open('booking90.com.csv', 'a', encoding= 'utf-8-sig', newline='') as f:
+        writer = csv.writer(f)
+        for i in review_dict:
+            if len(review_dict[i][0]) != 1:
+                writer.writerow([i,review_dict[i][0][0], review_dict[i][0][1], review_dict[i][1]]) 
+            else:   
+                writer.writerow([i,review_dict[i][0][0], ' ', review_dict[i][1]]) 
